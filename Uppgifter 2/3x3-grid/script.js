f4992d4511caeba20c07d9aae36edd8e7ea528c2
0ssi <oskar.fagernas@gmail.com>
2021-08-11 10:11:43 +0300

    e

---
+++ Uppgifter 2/3x3-grid/script.js
@@ -0,0 +1,11 @@
+function CUNT() {
+    for(var row=0;row<3;row++)
+    {
+        for(var col=0;col<3;col++)
+        {
+            CUNT(".row"+row).append("<div class='box'></div>");
+        }
+    }
+}
+
+CUNT();
---
+++ Uppgifter 2/functions/script.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("Please add the first number"));
+let num2 = parseInt(prompt("Please add the second number"));4
+
+
+
+if (num1, num2 != null) {
+    document.getElementById("answer").innerHTML = num1 + num2
+}
